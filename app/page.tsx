67fa9bab7ee915c3b2aa52859211ee09503852e9
iroid <iradtu2@gmail.com>
2024-05-30 20:50:57 +0300

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
@@ -29,6 +30,14 @@ export default function page() {
     "link_two": "https://desishub-docs.vercel.app/programming-tutorials/nextjs/next-auth",
     "topic": "Dom Manipulation"
 }`;
+  const single = `
+{
+  "title": "Assignment 3",
+  "description": "This is the descripton for Assignment 3",
+  "link_one": "https://desishub-docs.vercel.app/programming-tutorials/nextjs/next-auth",
+  "link_two": "https://desishub-docs.vercel.app/programming-tutorials/nextjs/next-auth",
+  "topic": "Dom Manipulation"
+}`;
   return (
     <div className="max-w-3xl mx-auto p-16">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -38,10 +47,13 @@ export default function page() {
         <h2 className=" py-4 text-blue-700 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Get all Assignments
         </h2>
-        <div className="mt-4 p-4 border border-gray-400">
+        <Link
+          href={"https://assignment-api-eta.vercel.app/api/assignments"}
+          className="mt-4 block p-4 border border-gray-400"
+        >
           [GET] https://assignment-api-eta.vercel.app/api/assignments
-        </div>
-        <p className="leading-7 [&:not(:first-child)]:mt-6 py-4">Response</p>
+        </Link>
+        <p className="leading-7 [&:not(:first-child)]:mt-6 py-2">Response</p>
         <div className="mt-4 p-4 border border-gray-400 overflow-hidden">
           <pre>
             <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
@@ -54,13 +66,19 @@ export default function page() {
         <h2 className=" py-4 text-blue-700 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Get a single Assignment
         </h2>
-        <div className="mt-4 p-4 border border-gray-400">
+        <Link
+          href={
+            "https://assignment-api-eta.vercel.app/api/assignments/assignement-2"
+          }
+          className="mt-4 p-4 block border border-gray-400"
+        >
           [GET] https://assignment-api-eta.vercel.app/api/assignments/[slug]
-        </div>
+        </Link>
+        <p className="leading-7 [&:not(:first-child)]:mt-6 py-2">Response</p>
         <div className="mt-4 p-4 border border-gray-400 overflow-hidden">
           <pre>
             <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-              {allAss}
+              {single}
             </code>
           </pre>
         </div>
@@ -69,9 +87,13 @@ export default function page() {
         <h2 className=" py-4 text-blue-700 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Create an Assignment
         </h2>
-        <div className="mt-4 p-4 border border-gray-400">
+        <Link
+          href={"https://assignment-api-eta.vercel.app/api/assignments"}
+          className="mt-4 block p-4 border border-gray-400"
+        >
           [POST] https://assignment-api-eta.vercel.app/api/assignments
-        </div>
+        </Link>
+        <p className="leading-7 [&:not(:first-child)]:mt-6 py-2">Body</p>
         <div className="mt-4 p-4 border border-gray-400 overflow-hidden">
           <pre>
             <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
