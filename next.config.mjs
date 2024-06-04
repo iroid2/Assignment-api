cabaa42a792fe7ab10c7a4b072e1ba50b0853299
iroid <iradtu2@gmail.com>
2024-06-04 17:41:17 +0300

    disabled cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
